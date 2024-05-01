1ae722b958b4bb6716bbd08f8f5845fb44b05e34
marteszibellina <166631214+marteszibellina@users.noreply.github.com>
2024-05-01 15:22:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 from datetime import datetime
+from names import user_data
 from schedule import schedule_lessons
 from schedule import schedule_all
 
@@ -22,16 +23,31 @@ print(current_weekday)
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, "Привет! Я буду твоим помощником!")
+    if message.chat.id in user_data and user_data[message.chat.id] == "Dmitry Sobolev":
+        bot.send_message(message.chat.id, f"Добрый день, {user_data[message.chat.id]}, Вы администратор")
+    else:
+        pass
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Ближайшее занятие")
     btn2 = types.KeyboardButton("Все занятия")
     btn3 = types.KeyboardButton("Контакты")
     markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id, "Выберите интересующую тебя опцию", reply_markup=markup)
+    bot.send_message(message.chat.id, "Выбери интересующую тебя опцию", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
+    if message.chat.id in user_data and user_data[message.chat.id] == "admin":
+        # Admin commands
+        if message.text == "/add_user" :
+            bot.send_message(message.chat.id, "Введите ID пользователя")
+            pass
+        elif message.text == "/remove_user" :
+            bot.send_message(message.chat.id, "Введите ID пользователя")
+            # Remove user command
+            pass
+        else :
+            bot.send_message(message.chat.id, "Неизвестная команда")
     if message.text == "Ближайшее занятие":
         bot.send_message(message.chat.id, f"Сегодня: \n{current_date} \n{current_weekday}")
         bot.send_message(message.chat.id, f"Занятие: {current_lesson(schedule_lessons, current_weekday)}")
---
+++ names.py
@@ -0,0 +1,5 @@
+user_data = {
+    3568804: "Dmitry Sobolev",
+    # Add user IDs as keys and their roles as values
+    # For example: 123456789: 'admin', 987654321: 'user'
+}
