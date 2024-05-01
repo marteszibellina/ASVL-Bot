f715d396d52071f63ff8832b890e32cf18c2f161
marteszibellina <166631214+marteszibellina@users.noreply.github.com>
2024-05-01 13:38:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from schedule import schedule_lessons
 from schedule import schedule_all
 
-
 token = "6328375734:AAExzH_QttvtLmDO4X2-axYoL2TyLWAHpuA"
 bot = telebot.TeleBot(token)
 
@@ -12,6 +11,7 @@ weekday = ["Понедельник", "Вторник", "Среда", "Четве
 current_date = datetime.now().strftime("%d.%m.%Y")
 current_weekday = weekday[datetime.now().weekday()]
 current_lesson = schedule_lessons[datetime.now().weekday()]
+
 def current_lesson(schedule_lessons, current_weekday):
     return schedule_lessons[datetime.now().weekday()]
 print(current_lesson(schedule_lessons, current_weekday))
@@ -23,17 +23,20 @@ print(current_weekday)
 def start_message(message):
     bot.send_message(message.chat.id, "Привет! Я буду твоим помощником!")
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Расписание")
-    btn2 = types.KeyboardButton("Контакты")
-    markup.add(btn1, btn2)
+    btn1 = types.KeyboardButton("Ближайшее занятие")
+    btn2 = types.KeyboardButton("Все занятия")
+    btn3 = types.KeyboardButton("Контакты")
+    markup.add(btn1, btn2, btn3)
     bot.send_message(message.chat.id, "Выберите интересующую тебя опцию", reply_markup=markup)
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    if message.text == "Расписание":
+    if message.text == "Ближайшее занятие":
         bot.send_message(message.chat.id, f"Сегодня: \n{current_date} \n{current_weekday}")
         bot.send_message(message.chat.id, f"Занятие: {current_lesson(schedule_lessons, current_weekday)}")
+    elif message.text == "Все занятия":
+        bot.send_message(message.chat.id, f"Все занятия в течение недели: {schedule_all}")
     elif message.text == "Контакты":
         bot.send_message(message.chat.id, "Контакты:"
                                           "\nАрт-студия Виталия Лещенко"
